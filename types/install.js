53975b27cfe0c4a37b48a7b7c3fb7eeca55d1cf1
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-02-08 08:13:06 -0800

    Remove registry enum, support language and installs on packages

---
+++ types/install.js
@@ -0,0 +1,12 @@
+export const Install = {
+  type: 'object',
+  properties: {
+    timestamp: {
+      type: 'number'
+    },
+    sessionId: {
+      description: 'Corresponds to session._id in the session table',
+      type: 'string'
+    }
+  }
+}
