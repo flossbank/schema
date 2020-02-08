283243d9894e1ffd8840cca7136ac1a59cc5dc2c
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-02-08 09:15:14 -0800

    Add count to package install

--- types/install.js
+++ types/install.js
@@ -7,6 +7,10 @@ export const Install = {
     sessionId: {
       description: 'Corresponds to session._id in the session table',
       type: 'string'
+    },
+    count: {
+      description: 'The number of times this package was installed during the session',
+      type: 'number'
     }
   }
 }
