2599d7907c880723ea871cc6b0df137ff2a4a1bc
Joel Wasserman <joel.wasserman8@gmail.com>
2020-05-03 09:24:07 -0700

    removed card on file from schema for advertisers

--- types/BillingInfo.js
+++ types/BillingInfo.js
@@ -3,10 +3,6 @@ const S = require('fluent-schema')
 const BillingInfo = S.object()
   .description('Public properties of the Billing Info object')
   .prop(
-    'cardOnFile',
-    S.boolean().description('Whether we have a card on file')
-  )
-  .prop(
     'last4',
     S.string().maxLength(4).description('Last 4 digits of the credit card on file (if present)')
   )
