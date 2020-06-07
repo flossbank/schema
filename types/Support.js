00c10761e9c3d27cb58ac667b9f7dcb1559c70d1
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-06 18:04:13 -0700

    added in customer feedback schema

---
+++ types/Support.js
@@ -0,0 +1,24 @@
+const S = require('fluent-schema')
+
+const CustomerFeedback = S.object()
+  .description('')
+  .prop(
+    'topic',
+    S.string().maxLength(128).required()
+  )
+  .prop(
+    'body',
+    S.string().maxLength(128).required()
+  )
+  .prop(
+    'name',
+    S.string().maxLength(128).required()
+  )
+  .prop(
+    'email',
+    S.string().maxLength(128).required()
+  )
+
+module.exports = {
+  CustomerFeedback,
+}
