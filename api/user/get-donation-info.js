824944ccf8867e930fb2c7f9b71085becedf5ea5
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-19 16:20:19 -0700

    change to number

--- api/user/get-donation-info.js
+++ api/user/get-donation-info.js
@@ -3,6 +3,6 @@ const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
   response: {
-    200: Base200WithData('amount', S.boolean())
+    200: Base200WithData('amount', S.number())
   }
 }
