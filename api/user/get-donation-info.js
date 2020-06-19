97156b6e548aafe893398157b5868d43a5b91ea6
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-19 16:42:25 -0700

    add last4 and renewal

--- api/user/get-donation-info.js
+++ api/user/get-donation-info.js
@@ -3,6 +3,6 @@ const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
   response: {
-    200: Base200WithData('amount', S.number())
+    200: Base200WithData('amount', S.number(), 'last4', S.string().maxLength(4), 'renewal', S.number())
   }
 }
