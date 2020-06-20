f48d91e37f407da970b08259ba042add33971823
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-19 17:01:30 -0700

    refactor

--- api/user/get-donation-info.js
+++ api/user/get-donation-info.js
@@ -1,8 +1,9 @@
 const S = require('fluent-schema')
+const { DonationInfoPublic } = require('../../types/BillingInfo')
 const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
   response: {
-    200: Base200WithData('amount', S.number(), 'last4', S.string().maxLength(4), 'renewal', S.number())
+    200: Base200WithData('donationInfo', DonationInfoPublic)
   }
 }
