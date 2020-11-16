f3c010658ad80d6961758396edc65b0eafa23977
Joel Wasserman <joel.wasserman8@gmail.com>
2020-11-16 09:48:00 -0800

    add host to query string of get org by name route

--- api/organization/get-by-name.js
+++ api/organization/get-by-name.js
@@ -3,7 +3,8 @@ const { OrganizationPublic } = require('../../types/Organization')
 const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
-  querystring: S.object().prop('name', S.string().maxLength(128)).required(),
+  querystring: S.object()
+    .prop('name', S.string().maxLength(128)).prop('host', S.string().maxLength(128)).required(),
   response: {
     200: Base200WithData('organization', OrganizationPublic)
   }
