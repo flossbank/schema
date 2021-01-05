79b17d10a6415180f4125ece7c2f7a84664d9ca2
Joel Wasserman <joel.wasserman8@gmail.com>
2021-01-05 15:16:18 -0800

    changed to returning full packages details on get

--- api/package/get.js
+++ api/package/get.js
@@ -1,10 +1,10 @@
 const S = require('fluent-schema')
-const { PackagePublic } = require('../../types/Package')
+const { PackageMaintainerProps } = require('../../types/Package')
 const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
   querystring: S.object().prop('id', S.string()).required(),
   response: {
-    200: Base200WithData('package', PackagePublic)
+    200: Base200WithData('package', PackageMaintainerProps)
   }
 }
