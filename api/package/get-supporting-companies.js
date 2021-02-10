c48f96d42ad2ce18512629a25965ab4075a3c98b
Joel Wasserman <joel.wasserman8@gmail.com>
2021-02-10 11:51:07 -0800

    use package id

--- api/package/get-supporting-companies.js
+++ api/package/get-supporting-companies.js
@@ -1,9 +1,9 @@
 const S = require('fluent-schema')
-const { CompaniesSupportingPackage } = require('../../types/Package')
+const { CompaniesSupportingPackage, PackageId } = require('../../types/Package')
 const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
-  querystring: S.object().prop('id', S.string()).required(),
+  querystring: S.object().prop('id', PackageId).required(),
   response: {
     200: Base200WithData('companies', CompaniesSupportingPackage)
   }
