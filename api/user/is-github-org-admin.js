70378a3037946280d4a9ef430c2356a1023106cb
Joel Wasserman <joel.wasserman8@gmail.com>
2021-03-25 21:49:46 -0700

    make orgId required

--- api/user/is-github-org-admin.js
+++ api/user/is-github-org-admin.js
@@ -3,7 +3,7 @@ const { Base200Response } = require('../../common/reply')
 const { OrganizationId } = require('../../types/Organization')
 
 module.exports = {
-  query: S.object().prop('orgId', OrganizationId),
+  query: S.object().prop('orgId', OrganizationId).required(),
   response: {
     200: Base200Response
   }
