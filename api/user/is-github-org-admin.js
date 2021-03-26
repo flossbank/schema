681c16a4fda5a8bab03ce98e59686a25ee0a6a8d
Joel Wasserman <joel.wasserman8@gmail.com>
2021-03-25 17:12:53 -0700

    add is github org admin schema

---
+++ api/user/is-github-org-admin.js
@@ -0,0 +1,10 @@
+const S = require('fluent-schema')
+const { Base200Response } = require('../../common/reply')
+const { OrganizationId } = require('../../types/Organization')
+
+module.exports = {
+  query: S.object().prop('orgId', OrganizationId),
+  response: {
+    200: Base200Response
+  }
+}
