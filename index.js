6a91694ae61d0f6b510746ca60b3017bb81a00c7
Joel Wasserman <joel.wasserman8@gmail.com>
2021-04-12 18:19:03 -0700

    add in rev share update schema

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ module.exports = {
   },
   package: {
     searchByName: require('./api/package/search-by-name'),
+    updateMaintainerRevenueShares: require('./api/package/update-maintainer-revenue-shares'),
     get: require('./api/package/get'),
     getSupportingCompanies: require('./api/package/get-supporting-companies'),
     update: require('./api/package/update'),
