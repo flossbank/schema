b491c5757784e55022d5c3351d405a99231a8a07
Peter Squicciarini <stripedpajamas273@gmail.com>
2021-05-18 17:57:52 -0700

    Add offset/limit to getDonationLedger route

--- api/organization/get-donation-ledger.js
+++ api/organization/get-donation-ledger.js
@@ -3,7 +3,10 @@ const { OrgDonationLedger, OrganizationId } = require('../../types/BillingInfo')
 const { Base200WithData } = require('../../common/reply')
 
 module.exports = {
-  querystring: S.object().prop('organizationId', OrganizationId).required(),
+  querystring: S.object()
+    .prop('organizationId', OrganizationId).required()
+    .prop('offset', S.integer().minimum(0))
+    .prop('limit', S.integer().minimum(1)),
   response: {
     200: Base200WithData('ledger', OrgDonationLedger)
   }
